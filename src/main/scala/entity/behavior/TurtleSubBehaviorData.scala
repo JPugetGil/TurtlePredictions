0e64d62647e735c81d75b69682c524ed5c6e756c
VIGLIANO LEA p1517348 <lea.vigliano@etu.univ-lyon1.fr>
2022-02-12 23:17:53 +0100

    Ajout documentation pour le sous-comportement des lunatiques

--- src/main/scala/entity/behavior/TurtleSubBehaviorData.scala
+++ src/main/scala/entity/behavior/TurtleSubBehaviorData.scala
@@ -1,5 +1,10 @@
 package entity.behavior
-
+/**
+  * Modélise un "sous-comportement" de tortue lunatique (régulière, fatiguée ou cyclique)/
+  * @param behaviorId type de comportement
+  * @param startTop top de début du comportement
+  * @param behaviorData données du comportement
+  */
 case class TurtleSubBehaviorData(behaviorId: Int, startTop: Int, temperature: Double, qualite: Double, behaviorData: TurtleBehaviorData) {
 
 }
